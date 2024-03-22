98f25b9a898f5607265aacbcdfb8007060a512f3
Claudia <clau050994@gmail.com>
2024-03-22 14:38:56 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className='container'>
+      <div className='mainBody'>
+        <h1>Veni Vici!</h1>
+        <p>Discover dogs from your wildest dreams!</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
